430c0b784e1198cc60e2d41fc5ecafedd0298eb2
Ubuntu <ubuntu@kubernetes.tuimac.private>
2021-10-10 08:32:02 +0000

    Creating local environment has been done

--- docker/dev/web-app/entrypoint.sh
+++ docker/dev/web-app/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /root/react-django/src/frontend
-
 npm start &
 
+cd /root/react-django/src/backend
 python3 manage.py runserver 0.0.0.0:8000 &
 
 /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
