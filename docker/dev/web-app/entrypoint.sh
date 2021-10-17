042b5ac7ec0a2bb39a1a2fac59f2a73e1fbeb6d1
Ubuntu <ubuntu@docker.tuimac.private>
2021-10-17 02:24:33 +0000

    fixed django migration

--- docker/dev/web-app/entrypoint.sh
+++ docker/dev/web-app/entrypoint.sh
@@ -5,6 +5,7 @@ npm start &
 
 cd /root/react-django/src/backend
 python3 manage.py migrate
+python3 manage.py migrate --fake
 python3 manage.py runserver 0.0.0.0:8000 &
 
 /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
